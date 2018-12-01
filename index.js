1aeeefdd8355ed91a4f3b2cfe21b92211fbe88e0
Spencer Allegrezza <sallegrezz@gmail.com>
2018-12-01 00:36:40 -0600

    feat(index): Serves directory file

--- index.js
+++ index.js
@@ -3,62 +3,58 @@ const path = require('path');
 const express = require('express');
 const markdown = require('markdown-it')('commonmark');
 const glob = require('fast-glob');
-const CONFIG = fs.readJSONSync('./server.config.json');
+const yaml = require('js-yaml');
 const dirTree = require('directory-tree');
-const walker = require('walk');
+
+const CONFIG = yaml.safeLoad(fs.readFileSync('./server.config.yaml'));
+
 
 let app = express();
 let directory = {}; // Looks like { "fileName" : "path/to/fileName.md" }
-// let fileTree = (() => {
-//   let asdf  = walker.walk("./files");
-//   asdf.on("file", (base, stats, next) => {
-
-//     let cat = new Object(base.split(path.sep).pop())
-//     Object.defineProperty(cat, )
-//     availableFiles.categories.push()
-
-//     next();
-//   })
-// });
-// console.log(fileTree())
 let fileTree = dirTree('./files');
-if (fileTree.length !== fs.readJSONSync("./server.directory.json").length) {
-  fs.writeFileSync('server.directory.json', fileTree)
-}
+
+// fs.remove(dirFile, (err) => {
+//   fs.writeJSONSync(`./${CONFIG.dirFolder}/${CONFIG.dirFile}`, fileTree);
+// });
+// let previousDir = fs.readJSONSync(`./${CONFIG.dirFolder}/${CONFIG.dirFile}`);
+// fs.writeJSONSync(`./${CONFIG.dirFolder}/${CONFIG.dirFile}`, fileTree);
 
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+// app.use(express.static('./directory'));
+app.get('/:subcategory', (req, res) => {
+    return new Promise((resolve, reject) => {
+        let subcategory = req.params.subcategory.toLowerCase();
+        let targetFilePath = directory[subcategory];
+        fs.readFile(targetFilePath)
+            .then(file => {
+                return res.json(markdown.render(file.toString()));
+            })
+            .catch(err => {
+                console.error(err);
+                return res.status(500).json(err);
+            });
+    });
 });
 
-app.get('/:subcategory', (req, res) => {
-  return new Promise((resolve, reject) => {
-
-    let subcategory = req.params.subcategory.toLowerCase();
-    let targetFilePath = directory[subcategory];
-    fs.readFile(targetFilePath).then((file) => {
-
-      return res.json(markdown.render(file.toString()));
-    }).catch(err => {
-      console.error(err);
-      return res.status(500).json(err);
-    })
-  })
-})
+const dirFile = yaml.safeLoad(fs.readFileSync(`./${CONFIG.dirFolder}/${CONFIG.dirFile}`));
 app.get('/', (req, res) => {
-  res.send(fs.readJSON('./server.config.json'))
-})
+  // Make this send 'dirFile' for better performance
+    res.send(yaml.safeLoad(fs.readFileSync(`./${CONFIG.dirFolder}/${CONFIG.dirFile}`, 'utf-8')));
+});
+
 // First, we find all the markdown files.
 glob([`./${CONFIG.folder}/*.md`, `./${CONFIG.folder}/**/*.md`]).then(mdFilePaths => {
-  mdFilePaths.forEach(file => {
-    directory[path.basename(file, '.md')] = file; // Gets the file name
-    // { "fileName" : "path/to/fileName.md" }
-  });
-
-  app.listen(CONFIG.port, () => {
-    return console.log(`Listening on ${CONFIG.port}`);
-  })
-
+    mdFilePaths.forEach(file => {
+        directory[path.basename(file, '.md')] = file; // Gets the file name
+        // { "fileName" : "path/to/fileName.md" }
+    });
+    app.listen(CONFIG.port, () => {
+        return console.log(`Listening on ${CONFIG.port}`);
+    });
 });
+
